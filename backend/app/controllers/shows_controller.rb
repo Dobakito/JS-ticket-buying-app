3df35882abe55aacf5d6eb86b47f34ec24a5f7e3
Dobakito <mdobak44@icloud.com>
2021-07-28 16:18:46 -0500

    updated controller methods
    added index and update actions to the respective controllers

--- backend/app/controllers/shows_controller.rb
+++ backend/app/controllers/shows_controller.rb
@@ -1,2 +1,11 @@
 class ShowsController < ApplicationController
+
+  def index
+    @shows = Show.where(genre_id: params[:genre_id])
+    render json: @shows, except: [:created_at, :updated_at]
+  end
+
+  def update
+    @shows.update(tickets_left: @show.tickets_left - 1 )
+  end
 end
