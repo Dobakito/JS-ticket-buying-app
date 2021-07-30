3cd6ae17a2b7f1346a4728a64abd36d4ae05c3eb
michael dobak <mdobak44@icloud.com>
2021-07-30 15:38:58 -0500

    building Show class functions
    building out the functions to manipulate the dom.

--- frontend/src/models/show.js
+++ frontend/src/models/show.js
@@ -8,14 +8,39 @@ class Show {
     shows.forEach(show => new Show(show))
   }
 
-  renderShowCards(){
-    //deletes html elements
-    let title = document.querySelector('#welcome-title')
-    let subTit = document.querySelector('#welcome-subtitle')
-    title.remove();
-    subTit.remove();
-    //adds html elements
+  // renderShopCart(){}
 
-    //render show cards
+  prepareDOM(){
+    let section = document.querySelector('#main')
+    let container = document.querySelector('#welcome')
+    let columns
+    let cardColumn
+      if (container) {
+        container.remove()
+        columns = document.createElement('div')
+        columns.className = "columns"
+        cardColumn = document.createElement('div')
+        cardColumn.className = "column is-two-thirds"
+        cardColumn.id = "removable"
+        section.append('columns')
+        columns.append('cardColumn')
+    } else {
+        columns = document.querySelector('.columns')
+        cardColumn = document.querySelector('#removable')
+        cardColumn.remove()
+        newCards = document.createElement('div')
+        newCards.className = "column is-two-thirds"
+        newCards.id = "removable"
+        columns.append('newCards')
+    }
+  }
+
+  renderShowCards(){
+    prepareDOM();
+    // renderShopCart()
+    
+
+
+
   }
 }
